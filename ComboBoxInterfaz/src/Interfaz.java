e3b6fb0a5ce952bbd9361fbd6f9cb0564227543b
Roman Najera <82838216+RomanSpanky@users.noreply.github.com>
2022-02-22 10:26:19 -0700

    Add files via upload

---
+++ ComboBoxInterfaz/src/Interfaz.java
@@ -0,0 +1,79 @@
+
+public class Interfaz extends javax.swing.JFrame {
+
+    public Interfaz() {
+        initComponents();
+        this.setLocationRelativeTo(Interfaz.this);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        etiResultado = new javax.swing.JLabel();
+        cboColores = new javax.swing.JComboBox<>();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        etiResultado.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+
+        cboColores.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Ninguno", "Violeta", "Morado", "Púrpura", "Lila" }));
+        cboColores.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cboColoresActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(76, 76, 76)
+                        .addComponent(cboColores, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(28, 28, 28)
+                        .addComponent(etiResultado, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(31, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(98, Short.MAX_VALUE)
+                .addComponent(cboColores, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(51, 51, 51)
+                .addComponent(etiResultado)
+                .addGap(53, 53, 53))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void cboColoresActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cboColoresActionPerformed
+        if (cboColores.getSelectedIndex() == 0) {
+            String mensaje = "No se eligió un color ";
+            etiResultado.setText(mensaje);
+        } else {
+            String mensaje = "El color elegido es ";
+
+            mensaje = mensaje + cboColores.getSelectedItem().toString();
+            etiResultado.setText(mensaje);
+        }
+    }//GEN-LAST:event_cboColoresActionPerformed
+
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Interfaz().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> cboColores;
+    private javax.swing.JLabel etiResultado;
+    // End of variables declaration//GEN-END:variables
+}
