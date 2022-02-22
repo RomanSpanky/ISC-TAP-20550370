2dd7aa55e614d2c4aa1927464a5360e6403063fb
Roman Najera <82838216+RomanSpanky@users.noreply.github.com>
2022-02-21 21:12:35 -0700

    Add files via upload

---
+++ HolaMundoInterfaz/src/Interfaz.java
@@ -0,0 +1,62 @@
+
+import javax.swing.JOptionPane;
+
+public class Interfaz extends javax.swing.JFrame {
+
+    public Interfaz() {
+        initComponents();
+        this.setLocationRelativeTo(Interfaz.this);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        Boton = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        Boton.setText("Mensaje");
+        Boton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(67, 67, 67)
+                .addComponent(Boton)
+                .addContainerGap(73, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(41, Short.MAX_VALUE)
+                .addComponent(Boton)
+                .addGap(37, 37, 37))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void BotonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonActionPerformed
+        JOptionPane.showMessageDialog(this, "Hola mundo!");
+    }//GEN-LAST:event_BotonActionPerformed
+
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Interfaz().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Boton;
+    // End of variables declaration//GEN-END:variables
+}
