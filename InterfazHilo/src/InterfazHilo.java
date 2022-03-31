1eff197edc6a29b151570d2c1a8d85cb7a3c0146
Roman Najera <82838216+RomanSpanky@users.noreply.github.com>
2022-03-30 19:08:35 -0700

    Add files via upload

--- InterfazHilo/src/InterfazHilo.java
+++ InterfazHilo/src/InterfazHilo.java
@@ -5,9 +5,11 @@ public class InterfazHilo {
     public static void main(String[] args) {
         
         Proceso1 H1 = new Proceso1();
+        Proceso1 H2 = new Proceso1();
+
         
         H1.start();
-        
+        H2.start();
     }
     
 }
